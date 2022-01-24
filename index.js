5c6e71b037dc517b2a84fd8277ce038db5042366
Vanessa Schneider <vanessajwade@gmail.com>
2022-01-24 16:43:53 -0500

    commit tree 71c36e70e8c63ea5f54c8360cca80a81d93bc31f

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
+document.addEventListener(DOMContentLoaded, function(){
+
+fetchBooks()
+})
+
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch ("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(results => {
+
+    results.forEach(book => renderBook(book))
+    
+  }
+    
+    
+    )
+}
+function renderBook(book) {
+const paragraph = document.creatElement("p")
+paragraph.innerText = book
+
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
+
+
+
+
+
+
